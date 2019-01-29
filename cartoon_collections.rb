1decad2f5010c2f15dc2a23a6d329bf379c2704f
Kyle Spencer <spencerk2@vcu.edu>
2019-01-29 02:03:45 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,14 +4,20 @@ def roll_call_dwarves(array)
   end
 end
 
+
+def summon_captain_planet(array)
+  capital_first_letter = []
+  
+  capital_first_letter = array.collect {|x| x.capitalize}
+  
+  exclimation_point = capital_first_letter.collect {|g| g + "!"}
+  
+  exclimation_point
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
-end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  array.any? {|x| x.length > 4}
 end
 
 def find_the_cheese# code an argument here
